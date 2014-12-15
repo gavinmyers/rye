461356a8966e266fa0a6098708861e6d1fd33b1b
gavin myers <gavin.myers@quantumretail.com>
2014-12-15 07:48:56 -0600

    adding back in keyboard controls

--- src/com/layout/boards/welcome.lua
+++ src/com/layout/boards/welcome.lua
@@ -1,13 +1,33 @@
 local board = require 'com.layout.board'
 local function main()
   local b = board:create("WELCOME")
+  b.playerX = 0
+  b.playerY = 0
+
   function b:_draw()
     love.graphics.setColor(255, 255, 255)
     local screenWidth, screenHeight = love.window.getDimensions()
     love.graphics.printf("\n******************\n   WELCOME TO LOAF \n******************\n\n [1] TUTORIAL \n\n [2] CONTINUE \n\n [X] QUIT", 0, 1, screenWidth, "center")
 
     love.graphics.setColor(0,255,255)
-    love.graphics.rectangle("fill",0,0,16,16) 
+    love.graphics.rectangle("fill",self.playerX,self.playerY,16,16) 
+  end
+
+  function b:_update(dt)
+    local speed = 120 
+    local dx, dy = 0, 0
+    if love.keyboard.isDown('right') then
+      dx = speed * dt
+    elseif love.keyboard.isDown('left') then
+      dx = -speed * dt
+    end
+    if love.keyboard.isDown('down') then
+      dy = speed * dt
+    elseif love.keyboard.isDown('up') then
+      dy = -speed * dt
+    end
+    self.playerX = self.playerX + dx
+    self.playerY = self.playerY + dy
   end
 
   function b:_keypressed(k)
