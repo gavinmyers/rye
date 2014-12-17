6b3479fdbc13b86243e6057a24c715e5f2752569
gavin myers <gavin.myers@quantumretail.com>
2014-12-16 21:37:21 -0600

    updating

--- src/com/rye.lua
+++ src/com/rye.lua
@@ -1,13 +1,32 @@
+local board = require 'com.layout.board'
+local map = require 'com.engine.map'
+--local engine = require 'com.layout.board'
+--local style = require 'com.layout.board'
+
 local function _rye()
   local rye = {}
   function rye:new()
     local r = {}
-    r.board = board:get("GAME")
-    r.engine = engine:get("GAME")
-    r.style = style:get("GAME")
+    r.board = board:new("GAME")
     r.map = map:new("CAVE")
+    --r.engine = engine:new("GAME")
+    --r.style = style:new("GAME")
+
+    function r:draw()
+      r.board:draw()
+    end
+
+    function r:update(dt)
+      r.board:update(dt)
+    end
+
+    function r:keypressed(k)
+      r.board:keypressed(k)
+    end
+
     return r
   end
+
   return rye
 end
 
--- src/main.lua
+++ src/main.lua
@@ -4,27 +4,28 @@ local global_lock = require 'lib.global_lock'
 local bump = require 'lib.bump'
 local shader = require 'lib.postshader'
 local light = require 'lib.light'
-local board = require 'com.layout.board'
+local rye = require 'com.rye'
 
 local maps = require.tree('com.engine.maps')
 local things = require.tree('com.engine.things')
 local tiles = require.tree('com.layout.tiles')
 local boards = require.tree('com.layout.boards')
 
-local welcome = board:new("GAME") 
+local rye = rye:new() 
+
 function love.load()
 end
 
 function love.keypressed(k)
-  welcome:keypressed(k)
+  rye:keypressed(k)
 end
 
 function love.update(dt)
-  welcome:update(dt)
+  rye:update(dt)
 end
 
 function love.draw()
-  welcome:draw()
+  rye:draw()
 end
 
 
